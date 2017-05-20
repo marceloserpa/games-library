17bc7ec927bb4348f110a4e1a9d85049f4f65233
marceloserpa <marcelo.serpa@ilegra.com>
2017-05-19 21:13:16 -0300

    Changed firebase url api

--- src/app/game/game.service.ts
+++ src/app/game/game.service.ts
@@ -8,6 +8,8 @@ import 'rxjs/Rx';
 @Injectable()
 export class GameService {
 
+  static readonly API_GAMES: string = "https://game-library-15c06.firebaseio.com/games.json";
+
   gameSelected = new EventEmitter<Game>();
 
   newGames:Subject<Game[]> = new Subject();
@@ -16,11 +18,8 @@ export class GameService {
 
   constructor(private rentService: RentService, private http: Http ){}
 
-
-
-
   fetchGames(){
-    this.http.get('https://game-library-6da4c.firebaseio.com/games.json')
+    this.http.get(GameService.API_GAMES)
     .map((resp: Response) => {
       const games: Game[] = resp.json();
       return games;
@@ -36,17 +35,16 @@ export class GameService {
   saveGame(game: Game){
     this.games.push(game);
     this.newGames.next(this.games);
-    return this.http.put('https://game-library-6da4c.firebaseio.com/games.json',  this.games);
+    return this.http.put(GameService.API_GAMES,  this.games);
 
   }
 
   updateGame(id:number, game: Game){
     this.games[id] = game;
     this.newGames.next(this.games);
-    return this.http.put('https://game-library-6da4c.firebaseio.com/games.json',  this.games);
+    return this.http.put(GameService.API_GAMES,  this.games);
 
   }
-
 
   getGame(id:number){
     return this.games[id];
